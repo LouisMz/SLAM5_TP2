453c01923688901dd7c9631d7b1a4de8aaa36897
LouisLEBG <louis.maze49@gmail.com>
2021-09-23 14:24:22 +0200

    Design FOrm1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,257 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblModele = new System.Windows.Forms.Label();
+            this.lblCategorie = new System.Windows.Forms.Label();
+            this.lblMarque = new System.Windows.Forms.Label();
+            this.lblTaille = new System.Windows.Forms.Label();
+            this.lblPrix = new System.Windows.Forms.Label();
+            this.lblCouleur = new System.Windows.Forms.Label();
+            this.lblNbExemp = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cbCategorie = new System.Windows.Forms.ComboBox();
+            this.tbModele = new System.Windows.Forms.TextBox();
+            this.tbCouleur = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.cbMarque = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.gbGestion = new System.Windows.Forms.GroupBox();
+            this.btnSupp = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.gpListe = new System.Windows.Forms.GroupBox();
+            this.gbGestion.SuspendLayout();
+            this.gpListe.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblModele
+            // 
+            this.lblModele.AutoSize = true;
+            this.lblModele.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblModele.Location = new System.Drawing.Point(7, 25);
+            this.lblModele.Name = "lblModele";
+            this.lblModele.Size = new System.Drawing.Size(74, 24);
+            this.lblModele.TabIndex = 0;
+            this.lblModele.Text = "Modèle";
+            // 
+            // lblCategorie
+            // 
+            this.lblCategorie.AutoSize = true;
+            this.lblCategorie.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategorie.Location = new System.Drawing.Point(7, 68);
+            this.lblCategorie.Name = "lblCategorie";
+            this.lblCategorie.Size = new System.Drawing.Size(91, 24);
+            this.lblCategorie.TabIndex = 1;
+            this.lblCategorie.Text = "Catégorie";
+            // 
+            // lblMarque
+            // 
+            this.lblMarque.AutoSize = true;
+            this.lblMarque.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMarque.Location = new System.Drawing.Point(7, 111);
+            this.lblMarque.Name = "lblMarque";
+            this.lblMarque.Size = new System.Drawing.Size(75, 24);
+            this.lblMarque.TabIndex = 2;
+            this.lblMarque.Text = "Marque";
+            // 
+            // lblTaille
+            // 
+            this.lblTaille.AutoSize = true;
+            this.lblTaille.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTaille.Location = new System.Drawing.Point(7, 153);
+            this.lblTaille.Name = "lblTaille";
+            this.lblTaille.Size = new System.Drawing.Size(55, 24);
+            this.lblTaille.TabIndex = 3;
+            this.lblTaille.Text = "Taille";
+            // 
+            // lblPrix
+            // 
+            this.lblPrix.AutoSize = true;
+            this.lblPrix.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrix.Location = new System.Drawing.Point(7, 192);
+            this.lblPrix.Name = "lblPrix";
+            this.lblPrix.Size = new System.Drawing.Size(42, 24);
+            this.lblPrix.TabIndex = 4;
+            this.lblPrix.Text = "Prix";
+            // 
+            // lblCouleur
+            // 
+            this.lblCouleur.AutoSize = true;
+            this.lblCouleur.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCouleur.Location = new System.Drawing.Point(7, 234);
+            this.lblCouleur.Name = "lblCouleur";
+            this.lblCouleur.Size = new System.Drawing.Size(77, 24);
+            this.lblCouleur.TabIndex = 5;
+            this.lblCouleur.Text = "Couleur";
+            // 
+            // lblNbExemp
+            // 
+            this.lblNbExemp.AutoSize = true;
+            this.lblNbExemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNbExemp.Location = new System.Drawing.Point(7, 281);
+            this.lblNbExemp.Name = "lblNbExemp";
+            this.lblNbExemp.Size = new System.Drawing.Size(202, 24);
+            this.lblNbExemp.TabIndex = 6;
+            this.lblNbExemp.Text = "Nombre d\'exemplaires";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(11, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Ajouter";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // cbCategorie
+            // 
+            this.cbCategorie.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbCategorie.FormattingEnabled = true;
+            this.cbCategorie.Location = new System.Drawing.Point(119, 64);
+            this.cbCategorie.Name = "cbCategorie";
+            this.cbCategorie.Size = new System.Drawing.Size(245, 28);
+            this.cbCategorie.TabIndex = 8;
+            // 
+            // tbModele
+            // 
+            this.tbModele.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbModele.Location = new System.Drawing.Point(119, 23);
+            this.tbModele.Name = "tbModele";
+            this.tbModele.Size = new System.Drawing.Size(245, 26);
+            this.tbModele.TabIndex = 9;
+            // 
+            // tbCouleur
+            // 
+            this.tbCouleur.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCouleur.Location = new System.Drawing.Point(119, 232);
+            this.tbCouleur.Name = "tbCouleur";
+            this.tbCouleur.Size = new System.Drawing.Size(245, 26);
+            this.tbCouleur.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(119, 151);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 26);
+            this.textBox1.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(119, 190);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(245, 26);
+            this.textBox2.TabIndex = 12;
+            // 
+            // cbMarque
+            // 
+            this.cbMarque.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbMarque.FormattingEnabled = true;
+            this.cbMarque.Location = new System.Drawing.Point(119, 107);
+            this.cbMarque.Name = "cbMarque";
+            this.cbMarque.Size = new System.Drawing.Size(245, 28);
+            this.cbMarque.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(232, 279);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(132, 26);
+            this.textBox3.TabIndex = 14;
+            // 
+            // gbGestion
+            // 
+            this.gbGestion.Controls.Add(this.textBox3);
+            this.gbGestion.Controls.Add(this.cbMarque);
+            this.gbGestion.Controls.Add(this.textBox2);
+            this.gbGestion.Controls.Add(this.textBox1);
+            this.gbGestion.Controls.Add(this.tbCouleur);
+            this.gbGestion.Controls.Add(this.tbModele);
+            this.gbGestion.Controls.Add(this.cbCategorie);
+            this.gbGestion.Controls.Add(this.button1);
+            this.gbGestion.Controls.Add(this.lblNbExemp);
+            this.gbGestion.Controls.Add(this.lblCouleur);
+            this.gbGestion.Controls.Add(this.lblPrix);
+            this.gbGestion.Controls.Add(this.lblTaille);
+            this.gbGestion.Controls.Add(this.lblMarque);
+            this.gbGestion.Controls.Add(this.lblCategorie);
+            this.gbGestion.Controls.Add(this.lblModele);
+            this.gbGestion.Location = new System.Drawing.Point(16, 21);
+            this.gbGestion.Name = "gbGestion";
+            this.gbGestion.Size = new System.Drawing.Size(431, 390);
+            this.gbGestion.TabIndex = 15;
+            this.gbGestion.TabStop = false;
+            this.gbGestion.Text = "Gestion des modèles et exemplaires";
+            // 
+            // btnSupp
+            // 
+            this.btnSupp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSupp.Location = new System.Drawing.Point(38, 185);
+            this.btnSupp.Name = "btnSupp";
+            this.btnSupp.Size = new System.Drawing.Size(108, 35);
+            this.btnSupp.TabIndex = 16;
+            this.btnSupp.Text = "Supprimer";
+            this.btnSupp.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(38, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(415, 134);
+            this.listBox1.TabIndex = 17;
+            // 
+            // gpListe
+            // 
+            this.gpListe.Controls.Add(this.listBox1);
+            this.gpListe.Controls.Add(this.btnSupp);
+            this.gpListe.Location = new System.Drawing.Point(489, 21);
+            this.gpListe.Name = "gpListe";
+            this.gpListe.Size = new System.Drawing.Size(493, 257);
+            this.gpListe.TabIndex = 18;
+            this.gpListe.TabStop = false;
+            this.gpListe.Text = "Liste des modèles et exemplaires";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(997, 450);
+            this.Controls.Add(this.gpListe);
+            this.Controls.Add(this.gbGestion);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.gbGestion.ResumeLayout(false);
+            this.gbGestion.PerformLayout();
+            this.gpListe.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblModele;
+        private System.Windows.Forms.Label lblCategorie;
+        private System.Windows.Forms.Label lblMarque;
+        private System.Windows.Forms.Label lblTaille;
+        private System.Windows.Forms.Label lblPrix;
+        private System.Windows.Forms.Label lblCouleur;
+        private System.Windows.Forms.Label lblNbExemp;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cbCategorie;
+        private System.Windows.Forms.TextBox tbModele;
+        private System.Windows.Forms.TextBox tbCouleur;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox cbMarque;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox gbGestion;
+        private System.Windows.Forms.Button btnSupp;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox gpListe;
     }
 }
 
---
+++ modele.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace tp1
+{
+    class modele
+    {
+        #region propriétés
+        private MySqlConnection myConnection;
+        private bool connopen = false;
+        private bool chargement = false;
+        private DataTable dT1 = new DataTable();
+        #endregion
+
+        #region accesseur
+        public bool Connopen { get => connopen; set => connopen = value; }
+        public bool Chargement { get => chargement; set => chargement = value; }
+        public DataTable DT1 { get => dT1; set => dT1 = value; }
+        #endregion
+
+        #region constructeur
+        #endregion
+
+        #region methode
+        public void seConnecter()
+        {
+            string myConnectionString = "Database=opticien_tl; Data Source=localhost; User Id=root; Password= ; ";
+            myConnection = new MySqlConnection(myConnectionString);
+            try // tentative
+            {
+                myConnection.Open();
+                connopen = true;
+            }
+            catch (Exception err)// gestion des erreurs
+            {
+                MessageBox.Show("Erreur ouverture bdd : " + err, "PBS connection",
+               MessageBoxButtons.OK, MessageBoxIcon.Error);
+                connopen = false;
+            }
+        }
+
+        public void sedeconnecter()
+        {
+            if (!connopen)
+                return;
+            try
+            {
+                myConnection.Close();
+                myConnection.Dispose();
+                connopen = false;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Erreur fermeture bdd : " + err, "PBS deconnection",
+               MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void charger(string requete, DataTable DT)
+        {
+            MySqlCommand command = myConnection.CreateCommand();
+            MySqlDataReader reader;
+            try
+            {
+                command.CommandText = requete;
+                reader = command.ExecuteReader();
+                DT.Load(reader);
+                chargement = true;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Erreur chargement dataTable: " + err, "PBS table",
+               MessageBoxButtons.OK, MessageBoxIcon.Error);
+                chargement = false;
+            }
+        }
+        #endregion
+    }
+}
+
